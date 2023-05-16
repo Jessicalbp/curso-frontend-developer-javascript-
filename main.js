f06196ef5f34c7b01d08983254d6d0ccd540761a
nikkibraiam <braiamsneiderp@gmail.com>
2023-05-16 15:06:22 -0500

    Commits

--- main.js
+++ main.js
@@ -1,42 +1,63 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuHambur = document.querySelector('.menu');
+const menuCarritoIncon = document.querySelector('.navbar-shopping-cart');
+const productDetailclose = document.querySelector('.product-detail-close');
 const desktopMenu  = document.querySelector('.desktop-menu');
-const menuCarritoIncon = document.querySelector('.navbar-shopping-cart')
-const menuHambur = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
-const aside = document.querySelector('.product-detail')
-const cardsContainer = document.querySelector('.cards-container')
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambur.addEventListener('click', toggleMobileMenu);
 menuCarritoIncon.addEventListener('click', toggleCarritoAside);
+productDetailclose.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
      }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
      }
+
+     closeProductDetailAside();
 
      mobileMenu.classList.toggle('inactive');
   }
-  
+
   function toggleCarritoAside () {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
      if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
      }
-     aside.classList.toggle('inactive');
+
+     const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+  
+     if (!isProductDetailClosed) {
+       productDetailContainer.classList.add('inactive'); 
+     }
+
+     shoppingCartContainer.classList.toggle('inactive');
+  }
+
+  function openProductDetailAside () {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive')
+  }
+
+  function closeProductDetailAside () {
+    productDetailContainer.classList.add('inactive');
   }
 
   const productList = [];
@@ -47,13 +68,28 @@ function toggleMobileMenu () {
   });
   productList.push({
     name: 'Computer',
-    price: 1700,
-    Image: 'https://www.unbosque.edu.co/sites/default/files/2020-07/computadores_0.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike"',
+    price: 170,
+    Image: 'https://www.unbosque.edu.co/sites/default/files/2020-07/computadores_0.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="computer"',
   });
   productList.push({
     name: 'Reloj',
     price: 750,
-    Image: 'https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/02/reloj-cocina-2620297.jpg?tf=3840x?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike"',
+    Image: 'https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/02/reloj-cocina-2620297.jpg?tf=3840x?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="reloj"',
+  });
+  productList.push({
+    name: 'Coat',
+    price: 300,
+    Image: 'https://i.pinimg.com/736x/6c/3a/e3/6c3ae3b75c930fa46d9f0a4b8b710fdf.jpg?tf=3840x?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="coat"',
+  });
+  productList.push({
+    name: 'Furnitures',
+    price: 1900,
+    Image: 'https://cdn.shopify.com/s/files/1/2393/8765/articles/556930138173_800x.jpg?v=1666722530?tf=3840x?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Furnitures"',
+  });
+  productList.push({
+    name: 'Toys',
+    price: 110,
+    Image: 'https://s3.abcstatics.com/media/familia/2021/12/12/JUGUETES-GENERO-kyoD--1248x698@abc.jpg?tf=3840x?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="toys"',
   });
 
   function renderProducts(arr) {
@@ -64,6 +100,7 @@ function toggleMobileMenu () {
     // product = {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.Image);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
